0ae7ed47d344e5d3ef97cfd1ff340f4331a4a8f7
Jonah-Y <jyamine46425@gmail.com>
2026-02-22 00:16:23 -0500

    added space to layer 4 and put right click on right hand

--- keymap.c
+++ keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
    KC_ESCAPE,      KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,LSFT(KC_MINS),KC_MINS,   KC_7,  KC_8, KC_9,KC_BSPC,
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
-    KC_TAB,  KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LSFT(KC_EQL),   KC_EQL,   KC_4, KC_5, KC_6,   KC_ENTER,
+    KC_TAB,  KC_SPACE, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,LSFT(KC_EQL),   KC_EQL,   KC_4, KC_5, KC_6,   KC_ENTER,
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
-    QK_BOOT,   KC_KP_ASTERISK, KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_KP_DOT,   KC_0,   KC_1, KC_2, KC_3,   TO(3),
+    QK_BOOT,   KC_KP_ASTERISK, KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_DOT,   KC_0,   KC_1, KC_2, KC_3,   TO(3),
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
 	  TO(0), KC_LGUI,              KC_RALT ,TO(2)
   ),
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
 	     TD(DANCE_56),   MS_LEFT,     MS_DOWN,     MS_RGHT,    MT(MOD_LGUI,KC_DEL),    TD(DANCE_60),   MT(MOD_RGUI,KC_DOWN), MT(MOD_RALT,KC_UP),   TD(DANCE_63),   KC_ENTER,     
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
-     TD(DANCE_58),   MS_BTN2,     MS_WHLU,   MS_WHLD,  MS_BTN1,      OSM(MOD_RCTL),  MS_ACL0,   MS_ACL1,   MS_ACL2,   KC_NO,
+     TD(DANCE_58),   MS_BTN2,     MS_WHLU,   MS_WHLD,  OSM(MOD_RCTL),       MS_BTN1, MS_ACL0,   MS_ACL1,   MS_ACL2,   KC_NO,
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
                         TO(0), KC_LGUI,                   KC_RALT, TO(1)
   )
